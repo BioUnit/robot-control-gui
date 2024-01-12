64d5bd1b34d103969f2226f5c54e95360335b28a
Andrei <wildwildandrew@gmail.com>
2024-01-12 18:25:37 +0400

    Added left,right,forw,back buttons with signals

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -1,5 +1,7 @@
 #include "mainwindow.h"
 #include "./ui_mainwindow.h"
+#include <QDebug>
+#include <QThread>
 
 MainWindow::MainWindow(QWidget *parent)
     : QMainWindow(parent)
@@ -12,3 +14,52 @@ MainWindow::~MainWindow()
 {
     delete ui;
 }
+
+void MainWindow::on_LeftButton_pressed()
+{
+    qDebug("Turn Left Pressed");
+    QThread::sleep(std::chrono::milliseconds{100});
+}
+
+void MainWindow::on_RightButton_pressed()
+{
+    qDebug("Turn Right Pressed");
+    QThread::sleep(std::chrono::milliseconds{100});
+}
+
+void MainWindow::on_BackwardButton_pressed()
+{
+    qDebug("Move Backward Pressed");
+    QThread::sleep(std::chrono::milliseconds{100});
+}
+
+void MainWindow::on_ForwardButton_pressed()
+{
+    qDebug("Move Forward Pressed");
+    QThread::sleep(std::chrono::milliseconds{100});
+}
+
+void MainWindow::on_ForwardButton_released()
+{
+    qDebug("Move Forward Released");
+    QThread::sleep(std::chrono::milliseconds{100});
+}
+
+void MainWindow::on_BackwardButton_released()
+{
+    qDebug("Move Backward Released");
+    QThread::sleep(std::chrono::milliseconds{100});
+}
+
+void MainWindow::on_LeftButton_released()
+{
+    qDebug("Turn Left Released");
+    QThread::sleep(std::chrono::milliseconds{100});
+}
+
+void MainWindow::on_RightButton_released()
+{
+    qDebug("Turn Right Released");
+    QThread::sleep(std::chrono::milliseconds{100});
+}
+
--- mainwindow.h
+++ mainwindow.h
@@ -17,6 +17,23 @@ public:
     MainWindow(QWidget *parent = nullptr);
     ~MainWindow();
 
+private slots:
+    void on_LeftButton_pressed();
+
+    void on_RightButton_pressed();
+
+    void on_BackwardButton_pressed();
+
+    void on_ForwardButton_pressed();
+
+    void on_ForwardButton_released();
+
+    void on_BackwardButton_released();
+
+    void on_LeftButton_released();
+
+    void on_RightButton_released();
+
 private:
     Ui::MainWindow *ui;
 };
